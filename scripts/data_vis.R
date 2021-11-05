86400d92e8736cce19c58782a0c09fb016459923
pracz_station <petermartonracz@gmail.com>
2021-11-05 07:00:58 +0100

    one more cor plot

--- scripts/data_vis.R
+++ scripts/data_vis.R
@@ -110,6 +110,34 @@ cors = dct %>%
     corrplot = map(cor, ~ corrplot::corrplot(., method = 'number'))
   )
 
+cors %<>% 
+  mutate(
+    long = map(cor, ~
+                 as.data.frame(.) %>% 
+                 rownames_to_column() %>% 
+                 pivot_longer(-rowname) %>% 
+                 rename(
+                   'var1' = rowname,
+                   'var2' = name,
+                   'cor' = value
+                 )
+               ) 
+  ) %>% 
+  select(group_type,long) %>% 
+  unnest(cols = c(long)) %>% 
+  mutate(pos = cor >= 0)
+  
+ac = ggplot(cors, aes(group_type, cor, fill = pos)) +
+  geom_col(position = 'dodge') +
+  facet_wrap( ~ var1 + var2 ) +
+  guides(fill = "none") +
+  ylab('rate of correlation') +
+  xlab('group') + 
+  theme_few() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggsave(ac, file = 'vis/all_cor.pdf', width = 12, height = 14)
+
 ## predictors across groups:
 dct %>% 
   pivot_longer(- group_type) %>% 
